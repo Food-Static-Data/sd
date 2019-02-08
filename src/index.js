c89ab8a44f316d888c6eb78c6fc7116a4ce703bb
Arthur Tkachenko <atherdon@gmail.com>
2019-02-08 03:54:00 +0200

    +++

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@
 //
 // import GeneratePDFButton from '~/GeneratePDFButton/GeneratePDFButton';
 
+//
+// import { readFileSync } from 'fs'
+//
+// import {fs} from 'fs';
+
+import fs from 'fs'
+
+var output = fs.readFileSync('./someData.txt')
+
+console.log(output)
 
 export {
-  // ClearCardsExample
+  // output
 }
