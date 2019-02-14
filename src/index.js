05990e4e3b76c17e9058ba591b830ed6b21d5450
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-14 03:32:35 +0200

    +++

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@
 //
 // import {fs} from 'fs';
 
-import fs   from 'fs'
+import { readFileSync }   from 'fs'
 import path from 'path'
 
 const file = path.resolve(__dirname, './data/Allergy/allergies.json')
-var output = fs.readFileSync(file)
+var output = readFileSync(file)
 // var output = fs.readFileSync('./someData.txt')
 
 console.log(output)
