c6536b324379f61407d2fcfc36e853553d5dc9d0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-08 21:26:20 +0200

    +

--- babel.config.js
+++ babel.config.js
@@ -19,14 +19,11 @@ const plugins = [
         '~': './src/data'
       }
     }
-    // require.resolve('@babel/plugin-module-resolver')
   ]
-
 ]
 
 module.exports = { presets, plugins }
 
-// "babel-plugin-module-resolver": "^3.1.1",
 // npx babel src --out-dir lib --ignore "src/**/*.spec.js","src/**/*.test.js"
 // https://gist.github.com/nodkz/41e189ff22325a27fe6a5ca81df2cb91
 // https://github.com/facebook/jest/issues/6662#issuecomment-429620436
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 var rollup = require('rollup')
 var babel = require('rollup-plugin-babel')
+// @TODO install that modules and connect them
 // rollup-plugin-json
 // rollup-plugin-node-resolve
 rollup.rollup({
-  entry: 'src/main.js',
+  entry: 'src/main.js', // 'src/index.js'
   plugins: [ babel() ]
 }).then(function (bundle) {
   bundle.write({
