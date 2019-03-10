60f1ff1ff70f58a5ea47b1a6a50c67b7a998bb65
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-10 17:09:17 +0200

    +

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@
 
 
 
-import { readFileSync }   from 'fs'
+import { readFileSync } from 'fs'
 import path from 'path'
 
 // const allergyFilePath = './data/Allergy/allergies.json';
@@ -23,9 +23,9 @@ const allergyFilePath = './data/Allergy/allergies.json';
 
 // const file = path.resolve(__dirname, './data/Allergy/allergies.json')
 // var output = readFileSync(file, 'utf8')
-var output = readFileSync(path.resolve(__dirname, './data/Allergy/allergies.json'), 'utf8')
+// var output = readFileSync(path.resolve(__dirname, './data/Allergy/allergies.json'), 'utf8')
 
-const json_or_not = JSON.parse(JSON.stringify(output))
+// const json_or_not = JSON.parse(JSON.stringify(output))
 // console.log(output)
 // console.log(JSON.parse(JSON.stringify(file)))
 // console.log(JSON.parse(JSON.stringify(file)))
@@ -36,5 +36,5 @@ const json_or_not = JSON.parse(JSON.stringify(output))
 export {
   // output,
   // file
-  json_or_not
+  // json_or_not
 }
