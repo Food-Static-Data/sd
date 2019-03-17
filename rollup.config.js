3493d3f94eb01a7a809b9aab1cf0f27921be6a17
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-17 04:09:58 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -71,19 +71,3 @@ export default {
     globals: {}
   }]
 }
-
-//
-// var rollup = require('rollup')
-// var babel = require('rollup-plugin-babel')
-// // @TODO install that modules and connect them
-// // rollup-plugin-json
-// // rollup-plugin-node-resolve
-// rollup.rollup({
-//   entry: 'src/main.js', // 'src/index.js'
-//   plugins: [ babel() ]
-// }).then(function (bundle) {
-//   bundle.write({
-//     dest: 'dist/bundle.js',
-//     format: 'umd'
-//   })
-// })
