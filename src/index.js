51be057611bccd2a28b7005e2bf07624ffa3807b
ronnie bhatt <ronniebhatt@ronnies-MacBook-Air.local>
2019-03-29 10:45:25 +0530

    export added

--- src/index.js
+++ src/index.js
@@ -24,3 +24,30 @@ import {
   groceryListWithUserRelations,
   dbIngredients
 } from './files'
+
+export default {
+  allergies,
+  course,
+  cuisine,
+  departments,
+  diet,
+  grocery,
+  holiday,
+  ingredients,
+  ingredients1,
+  ingredients3,
+  favorites,
+  items,
+  mealCalendar,
+  firstVeganGLMC,
+  measurements,
+  menu,
+  recipes,
+  nutritions,
+  nutritions2,
+  users,
+  userGrocery,
+  ultimateGroceryList,
+  groceryListWithUserRelations,
+  dbIngredients
+}
