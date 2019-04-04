cba22a93a43860d1ec29432512fc1ee975078f4c
Arthur Tkachenko <atherdon@gmail.com>
2019-04-04 10:14:40 +0300

    1

--- src/jsonlint2.js
+++ src/jsonlint2.js
@@ -1,13 +1,14 @@
-import shell from 'shelljs'
-import path from 'path'
-
-import { allergies } from './files.js'
-
-let filePath = path.resolve(__dirname, allergies)
-
-
-console.log(shell)
-console.log(allergies)
-console.log(filePath)
-
-shell.exec('jsonlint ' + filePath)
+import exec from 'shelljs.exec'
+console.log(exec)
+// import path from 'path'
+//
+// import { allergies } from './files.js'
+//
+// let filePath = path.resolve(__dirname, allergies)
+//
+//
+// console.log(shell)
+// console.log(allergies)
+// console.log(filePath)
+//
+// shell.exec('jsonlint ' + filePath)
