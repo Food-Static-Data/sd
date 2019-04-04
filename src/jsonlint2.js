1e882661de0f48e21d23b6459f1979cdd34aff20
Arthur Tkachenko <atherdon@gmail.com>
2019-04-04 10:04:08 +0300

    1

--- src/jsonlint2.js
+++ src/jsonlint2.js
@@ -1,8 +1,13 @@
 import shell from 'shelljs'
 import path from 'path'
 
-import allergies from './files.js'
+import { allergies } from './files.js'
 
 let filePath = path.resolve(__dirname, allergies)
+
+
+console.log(shell)
+console.log(allergies)
+console.log(filePath)
 
 shell.exec('jsonlint ' + filePath)
