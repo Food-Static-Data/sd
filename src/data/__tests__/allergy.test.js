0037d5d9667a63538d713ab8bd2badec3f8e1831
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-10 02:52:42 +0400

    test allergy
    test allergy

--- src/data/__tests__/allergy.test.js
+++ src/data/__tests__/allergy.test.js
@@ -2,8 +2,8 @@
 'use strict'
 import allergies from '~/Allergy/allergies.json'
 
-describe('allergies data files returns array', () => {
-  test('all tests depended on allergies are here', () => {
+describe('tests for allergy', () => {
+  test('allergies data files returns array', () => {
     expect(allergies).not.toBe('')
   })
 })
