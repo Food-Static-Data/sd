eda9e2db28ff0d396c18fa49419b654dd0647ee1
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-10 00:46:10 +0400

    adding test for allergy
    adding test for allergy

--- src/data/__tests__/allergy.test.js
+++ src/data/__tests__/allergy.test.js
@@ -3,6 +3,17 @@
 import allergies from '~/Allergy/allergies.json'
 
 describe('allergies data files returns array', () => {
+  test('test for Allergy', () => {
+    expect(allergies).not.toBe('')
+  })
+})
+
+
+
+'use strict'
+import allergies from '~/Allergy/allergies.json'
+
+describe('allergies data files returns array', () => {
   test('xxxx', () => {
     expect(allergies).not.toBe('')
   })
