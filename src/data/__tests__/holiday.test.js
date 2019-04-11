d422573408836ae07d4572bc65e1167099c33dea
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-12 01:02:53 +0400

    Update holiday.test.js
    update holiday.test.js

--- src/data/__tests__/holiday.test.js
+++ src/data/__tests__/holiday.test.js
@@ -3,7 +3,7 @@
 import holidays from '~/Holiday/holidays.json'
 
 describe('holidays data files returns array', () => {
-  test('xxxx', () => {
+  test('these tests prevent any issues and problems, also to break the structure of holiday', () => {
     expect(holidays).not.toBe('')
   })
 })
