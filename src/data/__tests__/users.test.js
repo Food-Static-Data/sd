4073a35e9366e85e99a3a1e6ff9dbe513dfb8518
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-12 01:17:29 +0400

    Update users.test.js
    update users

--- src/data/__tests__/users.test.js
+++ src/data/__tests__/users.test.js
@@ -3,7 +3,7 @@
 import users from '~/Users/users.json'
 
 describe('users data files returns array', () => {
-  test('xxxx', () => {
+  test('these tests prevent any issues and problems, also to break the structure of users', () => {
     expect(users).not.toBe('')
   })
 })
