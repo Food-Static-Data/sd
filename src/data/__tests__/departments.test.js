9cb0a2857c5b87d2b220ea8c6238e7a119cb133f
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-12 00:56:26 +0400

    update departments
    update departments

--- src/data/__tests__/departments.test.js
+++ src/data/__tests__/departments.test.js
@@ -3,7 +3,7 @@
 import departments from '~/Departments/departments.json'
 
 describe('departments data files returns array', () => {
-  test('xxxx', () => {
+  test('these tests prevent any issues and problems, also to break the structure of departments', () => {
     expect(departments).not.toBe('')
   })
 })
