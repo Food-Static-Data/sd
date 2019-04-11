02cb83a4de0dd1e24a14d019cbe162eb51b60e2f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-04-12 01:48:36 +0300

    Update attribute2.0.test.js

--- src/data/__tests__/attribute2.0.test.js
+++ src/data/__tests__/attribute2.0.test.js
@@ -1,9 +1,9 @@
 /* global test, describe, it, expect */
 'use strict'
-import attribute2.0 from '~/Attribute2.0/attribute2.0.json'
+import attributes from '~/Attribute2.0/attribute2.0.json'
 
-describe('attribute2.0 data files returns array', () => {
-  test('these tests prevent any issues and problems, also to break the structure of attribute2.0', () => {
-    expect(attribute2.0).not.toBe('')
+describe('attributes data files returns array', () => {
+  test('these tests prevent any issues and problems, also to break the structure of attributes', () => {
+    expect(attributes).not.toBe('')
   })
 })
