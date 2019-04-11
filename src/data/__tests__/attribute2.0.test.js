b322a0912a16178ac75398f16a7e4f4292b7fa29
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-12 02:41:34 +0400

    Update attribute2.0.test.js
    update attribute2.0

--- src/data/__tests__/attribute2.0.test.js
+++ src/data/__tests__/attribute2.0.test.js
@@ -1,9 +1,9 @@
 /* global test, describe, it, expect */
 'use strict'
-import allergies from '~/Allergy/allergies.json'
+import attribute2.0 from '~/Attribute2.0/attribute2.0.json'
 
 describe('attribute2.0 data files returns array', () => {
   test('these tests prevent any issues and problems, also to break the structure of attribute2.0', () => {
-    expect(allergies).not.toBe('')
+    expect(attribute2.0).not.toBe('')
   })
 })
