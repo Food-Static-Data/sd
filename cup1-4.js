d5241803c81b5f64eb68a51e359d562455cd66d7
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-16 19:47:08 +0400

    Create cup1-4.js
    adding cup1-4

---
+++ cup1-4.js
@@ -0,0 +1,7 @@
+
+    let cup1_4 = {                                //  1/4 cup
+          americanStandart: "4 tablespoons",
+          americanStandartOunces: "2 ounces",
+          volume: "60 milliliters",
+          weight: "56.7 grams"
+        };
