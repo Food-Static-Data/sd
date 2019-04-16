9e1eb45df195eb77867eefdf719dfbd51cb0a3f0
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-16 19:55:03 +0400

    Create gallon1.js
    adding gallon1

---
+++ gallon1.js
@@ -0,0 +1,7 @@
+
+   let gallon1 = {
+         americanStandart: "16 cups,  4 quarts",
+         americanStandartOunces: "128 ounces",
+         volume: "3.79 liters",
+         weight: null
+       };
