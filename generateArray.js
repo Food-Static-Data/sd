1ffd8f7a9c76ae8fdeab356aab65ab214ab680d0
Vadim <vadim.putrov@gmail.com>
2019-04-16 18:19:46 +0300

    add usersGrocery

--- generateArray.js
+++ generateArray.js
@@ -1,15 +1,49 @@
+const uuidv1 = require('uuid/v1')
 
+const filePath = require('./files')
 
-var users = ["2"];
-var grocery = [];
+var users = require(filePath['usersFilePath'])
+var grocery = require(filePath['groceryFilePath'])
+
 var i = 1;
-var favorites = users.map(user =>{
-    var id = i++;
-    return {
-        "ingredient_id": id,
-        "userId": id,
-        "favs": "desc for department"+ id,
-        "grocery_id": id
-    }
-})
-console.log(favorites);
+const __generateId = () => {
+    return uuidv1();
+}
+
+function generateArrWithId(data, idName) {
+
+    return data.map(elem =>{
+        return {
+            ...elem,
+            [idName]: __generateId()
+        }
+    })
+}
+
+function usersGrocery(){
+    var groceryId = generateArrWithId(grocery,"grocery_id");
+
+    var usersId = generateArrWithId(users,"user_id");
+
+    return usersId.map(user => {
+        return{
+            "user_id": user['user_id'],
+            // one grocery id for all users
+            "grocery_id": groceryId[0]['grocery_id']
+        }
+    })
+}
+// var favorites = users.map(user => {
+    
+
+//     return {
+//         "ingredient_id": id,
+//         "userId": id,
+//         "favs": "desc for department" + id,
+//         "grocery_id": id
+//     }
+// })
+// console.log(favorites);
+// var usersWithId = 
+console.log(usersGrocery());
+// console.log(generateArrWithId(users));
