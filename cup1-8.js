ac043743ab569e196f61df4c18d0f47e87297da9
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-16 19:48:22 +0400

    Create cup1-8.js
    adding cup1-8

---
+++ cup1-8.js
@@ -0,0 +1,7 @@
+
+   let cup1_8 = {                                //  1/8 cup
+         americanStandart: "2 tablespoons",
+         americanStandartOunces: "1 ounce",
+         volume: "30 milliliters",
+         weight: "28.3 grams"
+       };
