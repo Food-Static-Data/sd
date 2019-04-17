b0d4153e58fb05ede66b5950ee6898c367d0eef7
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-17 21:57:07 +0400

    Create dash.test.js
    testing dash

---
+++ src/data/__tests__/dash.test.js
@@ -0,0 +1,9 @@
+/* global test, describe, it, expect */
+'use strict'
+import dash from '~/GroceriStar/cd/dash.js'
+
+describe('this test prevents to any issues and problems, also to break the structure of cuisines data dash', () => {
+  test('cookng-times data files returns object', () => {
+    expect(dash).not.toBe('')
+  })
+})
