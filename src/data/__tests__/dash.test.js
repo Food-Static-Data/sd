6ed3cc9139c5c03a3b63b27487cdeed8323d4f0d
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-17 22:17:31 +0400

    Update dash.test.js

--- src/data/__tests__/dash.test.js
+++ src/data/__tests__/dash.test.js
@@ -2,7 +2,7 @@
 'use strict'
 import dash from '~/GroceriStar/cd/dash.js'
 
-describe('this test prevents to any issues and problems, also to break the structure of cuisines data dash', () => {
+describe('this test prevents to any issues and problems, also to break the structure of dash data', () => {
   test('dash data files returns object', () => {
     expect(dash).not.toBe('')
   })
