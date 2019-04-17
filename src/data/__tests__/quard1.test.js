f546d85cca68d1e4d03ccfdacb3c94905d6015db
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-17 22:51:20 +0400

    Update quard1.test.js

--- src/data/__tests__/quard1.test.js
+++ src/data/__tests__/quard1.test.js
@@ -3,7 +3,7 @@
 import quart1 from '~/GroceriStar/cd/quart1.js'
 
 describe('this test prevents to any issues and problems, also to break the structure of quart1 data', () => {
-  test('cup3_4 data files returns object', () => {
+  test('quart1 data files returns object', () => {
     expect(quart1).not.toBe('')
   })
 })
