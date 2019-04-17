d31bfd89ed12906bd084aee4e45bf2e90f3fe211
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-17 21:55:16 +0400

    Create cup3_4.test.js
    testing cup3_4

---
+++ src/data/__tests__/cup3_4.test.js
@@ -0,0 +1,9 @@
+/* global test, describe, it, expect */
+'use strict'
+import cup3_4 from '~/GroceriStar/cd/cup3_4.js'
+
+describe('this test prevents to any issues and problems, also to break the structure of cuisines data cup3_4', () => {
+  test('cookng-times data files returns object', () => {
+    expect(cup3_4).not.toBe('')
+  })
+})
