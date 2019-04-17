d6866e11e83e60283cbeca66ca9f058bc78ae71a
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-17 22:01:57 +0400

    Update cup3_4.test.js

--- src/data/__tests__/cup3_4.test.js
+++ src/data/__tests__/cup3_4.test.js
@@ -3,7 +3,7 @@
 import cup3_4 from '~/GroceriStar/cd/cup3_4.js'
 
 describe('this test prevents to any issues and problems, also to break the structure of cuisines data cup3_4', () => {
-  test('cookng-times data files returns object', () => {
+  test('cup3_4 data files returns object', () => {
     expect(cup3_4).not.toBe('')
   })
 })
