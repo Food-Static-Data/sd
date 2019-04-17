29d00f27127885c7d633203e6b5ba580f74b2c13
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-17 22:32:28 +0400

    Update gallon1.test.js

--- src/data/__tests__/gallon1.test.js
+++ src/data/__tests__/gallon1.test.js
@@ -2,7 +2,7 @@
 'use strict'
 import gallon1 from '~/GroceriStar/cd/gallon1.js'
 
-describe('this test prevents to any issues and problems, also to break the structure of cuisines data gallon1', () => {
+describe('this test prevents to any issues and problems, also to break the structure of gallon1 data', () => {
   test('cookng-times data files returns object', () => {
     expect(gallon1).not.toBe('')
   })
