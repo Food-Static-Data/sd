e00dff417521cc36604cfc3d9017da4766db71f8
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-23 18:42:54 +0400

    Update cooking_times.test.js
    upd

--- src/data/__tests__/cooking_times.test.js
+++ src/data/__tests__/cooking_times.test.js
@@ -1,6 +1,22 @@
 /* global test, describe, it, expect */
 'use strict'
-import cooking_times from '../../../cooking_times'
+import {
+  frenchBeans,
+  sprouts,
+  jacketPotato,
+  sweetPotato,
+  kale,
+  leek,
+  broccoli,
+  carrots,
+  mushrooms,
+  onions_shallots,
+  parsnips,
+  peppers,
+  marrow_squash,
+  sweetcorn,
+  courgettes
+} from '../../../cooking_times'
 
 describe('this test prevents to any issues and problems, also to break the structure of cooking_times data', () => {
   test('cooking_times data files returns object', () => {
