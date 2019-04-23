c43c069004be95265af883553235b36ac91ee3b0
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-23 15:47:22 +0400

    Update cookingTimes.test.js

--- src/data/__tests__/cookingTimes.test.js
+++ src/data/__tests__/cookingTimes.test.js
@@ -2,7 +2,7 @@
 'use strict'
 import cooking-times from '../cd/cooking-times.js'
 
-describe('this test prevents to any issues and problems, also to break the structure of cuisines data cooking-times', () => {
+describe('this test prevents to any issues and problems, also to break the structure of cooking-times data', () => {
   test('cookng-times data files returns object', () => {
     expect(cooking-times).not.toBe('')
   })
