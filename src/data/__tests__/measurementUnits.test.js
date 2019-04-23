52b5160d437a17d4c769a9ceb42a7b66ff10433e
wahaj-47 <wahajhssn@gmail.com>
2019-04-23 21:29:28 +0500

    Tests for generated jsons

--- src/data/__tests__/measurementUnits.test.js
+++ src/data/__tests__/measurementUnits.test.js
@@ -1,9 +1,17 @@
 /* global test, describe, it, expect */
 'use strict'
 import measurementUnits from '~/Units/measurementUnits.json'
+import generatedMeasurementUnits from '~/MeasurementUnits/measurementUnits.json'
+
 
 describe('users data files returns array', () => {
   test('these tests prevent any issues and problems, also to break the structure of measurementUnits', () => {
     expect(measurementUnits).not.toBe('')
   })
+})
+
+describe('users data files returns array', () => {
+    test('these tests prevent any issues and problems, also to break the structure of measurementUnits', () => {
+      expect(generatedMeasurementUnits).not.toBe('')
+    })
 })
