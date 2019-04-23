c1a43cabb33d08089af589f72fc58438244e7aa0
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-23 15:40:37 +0400

    Update cookingTimes.test.js
    upd

--- src/data/__tests__/cookingTimes.test.js
+++ src/data/__tests__/cookingTimes.test.js
@@ -1,6 +1,6 @@
 /* global test, describe, it, expect */
 'use strict'
-import cooking-times from '~/GroceriStar/cd/cooking-times.js'
+import cooking-times from '../cd/cooking-times.js'
 
 describe('this test prevents to any issues and problems, also to break the structure of cuisines data cooking-times', () => {
   test('cookng-times data files returns object', () => {
