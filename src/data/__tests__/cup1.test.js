7c025d4914f140f2a189c5d4a2d292292cc21106
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-24 21:53:49 +0400

    Update cup1.test.js

--- src/data/__tests__/cup1.test.js
+++ src/data/__tests__/cup1.test.js
@@ -1,8 +1,8 @@
 /* global test, describe, it, expect */
 'use strict'
-import cup1 from '~/GroceriStar/cd/cup1.js'
+import cup1 from '../../../cup1'
 
-describe('this test prevents to any issues and problems, also to break the structure of cuisines data cup1', () => {
+describe('this test prevents to any issues and problems, also to break the structure of cup1 data', () => {
   test('cup1 data files returns object', () => {
     expect(cup1).not.toBe('')
   })
