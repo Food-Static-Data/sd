9dc2e0e3ccc02d07ec57d72c9369ba58445f1a7c
Wahaj Hussain <wahajhssn@gmail.com>
2019-04-24 21:46:02 +0500

    Update measurementSystems.test.js

--- src/data/__tests__/measurementSystems.test.js
+++ src/data/__tests__/measurementSystems.test.js
@@ -1,7 +1,7 @@
 /* global test, describe, it, expect */
 'use strict'
 import measurementSystems from '~/Units/measurementSystems.json'
-import generatedMeasurementSystems from '~/MeasurementSystems/measurementSystems.json'
+import generatedMeasurementSystems from '~/MeasurementSystem/measurementSystem.json'
 
 describe('users data files returns array', () => {
   test('these tests prevent any issues and problems, also to break the structure of measurementSystems', () => {
