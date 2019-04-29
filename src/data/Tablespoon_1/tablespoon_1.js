505e2de2126ac45c5382c8cdc9363cbe98c6a0c2
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-30 00:55:19 +0400

    created in json

--- src/data/Tablespoon_1/tablespoon_1.js
+++
@@ -1,9 +0,0 @@
-
-  let tablespoon_1 = {
-        americanStandart: "3 teaspoons",
-        americanStandartOunces: "1/2 ounce",
-        volume: "15 milliliters",
-        weight: "14.3 grams"
-      };
-
-   export default tablespoon_1;
