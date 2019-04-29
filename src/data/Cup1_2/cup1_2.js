7b88067027890a754265f45fc98e655abb16d324
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-30 00:43:01 +0400

    created in json

--- src/data/Cup1_2/cup1_2.js
+++
@@ -1,9 +0,0 @@
-
-let cup1_2 = {
-         americanStandart: "8 tablespoons",
-         americanStandartOunces: "4 ounces",
-         volume: "120 milliliters",
-         weight: "113.4 grams, 1/4 pound"
-       };
-
-export default cup1_2;
