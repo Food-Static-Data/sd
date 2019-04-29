65728c1358e89bc72aa92d5c499b5054fd24f7bc
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-30 00:49:18 +0400

    created in json

--- src/data/Dash/dash.js
+++
@@ -1,8 +0,0 @@
-
-   let dash = {
-         americanStandart: "1/8 teaspoon",
-         americanStandartOunces: null,
-         volume: null,
-         weight: null
-       };
-   export default dash;
