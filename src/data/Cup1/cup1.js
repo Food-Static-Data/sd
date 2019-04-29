76ed3f6e52a6de0c0c32129b89a4256dec82c54c
elnur004 <47173552+elnur004@users.noreply.github.com>
2019-04-29 23:07:00 +0400

    upd

--- src/data/Cup1/cup1.js
+++ src/data/Cup1/cup1.js
@@ -1,8 +1,10 @@
 
-
    let cup1 = {
          americanStandart: "16 tablespoons",
          americanStandartOunces: "8 ounces",
          volume: "240 milliliters",
          weight: "225 grams,  1/2 pound"
        };
+
+   export default cup1_2;
+
