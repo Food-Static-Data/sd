1b56f521e5768f2ced2ed39c24ffbf325385fe32
wahaj-47 <wahajhssn@gmail.com>
2019-05-01 12:11:59 +0500

    Fixed paths

--- src/data/__tests__/allergy.test.js
+++ src/data/__tests__/allergy.test.js
@@ -1,6 +1,6 @@
 /* global test, describe, it, expect */
 'use strict'
-import allergies from '../../files.js'
+import {allergiesFilePath as allergies} from '../../../files.js'
 
 describe('tests for allergy', () => {
   test('allergies data files returns array', () => {
