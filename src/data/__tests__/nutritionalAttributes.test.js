210bed2e5b1edbdc0e69484dfaaa49791bc721a4
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-05-14 23:46:02 +0300

    travis CI build helper

--- src/data/__tests__/nutritionalAttributes.test.js
+++ src/data/__tests__/nutritionalAttributes.test.js
@@ -1,7 +1,7 @@
 /* global test, describe, it, expect */
 'use strict'
-const {nutritions2FilePath} = reuqire('../../../files.js')
-const nutritionalAttributes = reuqire(nutritions2FilePath)
+const {nutritions2FilePath} = require('../../../files.js')
+const nutritionalAttributes = require(nutritions2FilePath)
 
 describe('nutritionalAttributes data files returns array', () => {
     test('these tests prevent any issues and problems, also to break the structure of nutritionalAttributes', () => {
