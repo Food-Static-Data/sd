acd4ffb4c6a15f020597c0662a97368e7c0d5721
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-20 14:25:09 +0100

    Update cup1_2.test.js

--- src/data/__tests__/cup1_2.test.js
+++ src/data/__tests__/cup1_2.test.js
@@ -1,9 +1,9 @@
 /* global describe, it, expect */
 'use strict'
-const { cup12 } = require('../../../files.js')
+const staticData= require('../../../filesObjects.js')
 
 describe('this test prevents to any issues and problems, also to break the structure of cup1_2 data', () => {
   it('cup1_2 data files returns object', () => {
-    expect(cup12).not.toBe('')
+    expect(staticData.cup12).not.toBe('')
   })
 })
