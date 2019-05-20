ef38604dbd7b8696086c946a64dd94e05f35eeae
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-20 14:48:22 +0100

    Update pint1.test.js

--- src/data/__tests__/pint1.test.js
+++ src/data/__tests__/pint1.test.js
@@ -1,9 +1,9 @@
 /* global describe, it, expect */
 'use strict'
-const { pint1 } = require('../../../files.js')
+const staticData = require('../../../filesObjects.js')
 
 describe('this test prevents to any issues and problems, also to break the structure of pint1 data', () => {
   it('pint1 data files returns object', () => {
-    expect(pint1).not.toBe('')
+    expect(staticData.pint1).not.toBe('')
   })
 })
