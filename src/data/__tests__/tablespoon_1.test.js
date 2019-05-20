a290c3769e019973f333c860f6af0168fd56ab27
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-20 14:50:15 +0100

    Update tablespoon_1.test.js

--- src/data/__tests__/tablespoon_1.test.js
+++ src/data/__tests__/tablespoon_1.test.js
@@ -1,9 +1,9 @@
 /* global describe, it, expect */
 'use strict'
-const { tablespoon1 } = require('../../../files.js')
+const staticData= require('../../../filesObjects.js')
 
 describe('this test prevents to any issues and problems, also to break the structure of tablespoon_1 data', () => {
   it('tablespoon_1 data files returns object', () => {
-    expect(tablespoon1).not.toBe('')
+    expect(staticData.tablespoon1).not.toBe('')
   })
 })
