396b8364c029f79e912700e886023c50d65b83b5
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-25 02:49:02 +0100

    Update groceristar.teszt.js
    copying the test file from groceristar-fetch to groceristar in generator folder.issue#243

--- generator/projects/GroceriStar/test/groceristar.teszt.js
+++ generator/projects/GroceriStar/test/groceristar.teszt.js
@@ -112,3 +112,23 @@
 //     }
 //   })
 // })
+
+
+const groceristar = require('../groceristar.js')
+  
+describe('getKeyArrayDepending method', () => {
+    
+const result = groceristar.getKeyArrayDepAndIng()
+    
+  test('testing the method returns an array', () => {
+        
+    expect(Array.isArray(result)).toBe(true)
+     
+   })
+
+  test('testing the returned array is not empty',() => {
+
+    expect(result).not.toBe('')
+
+   }) 
+})
