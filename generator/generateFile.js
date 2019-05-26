99324e69152e02c3054ff156b9ae7a74210cb8b2
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-05-26 22:21:17 +0300

    todos

--- generator/generateFile.js
+++ generator/generateFile.js
@@ -1,12 +1,13 @@
 var writeInFile = require('./writeFile')
-// var { chickenKyiv } = require('@groceristar/groceristar-fetch')
-// const uuidv1 = require('uuid/v1')
 
 var { getMenuGenerator } = require('./generateArray')
 
+// right now it's used only for getMenuGenerator i think it wrong.
+// and we need to regenerate all files from scratch each time. especially if we re creating a new files or make cnahnges at
+// @TODO big issue
 function generateFile () {
   var fileName = 'generatedMenu'
-  var path = './output/' + fileName + '.json'
+  var path = './output/' + fileName + '.json' //i don't like this paths. at least we can move first part outside - aka output...
   var data = getMenuGenerator(2)
 
   writeInFile.writeFile(path, data)
