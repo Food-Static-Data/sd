0e42d07896419e62adb50535db5af0baedb84e81
EbrahimKreem <ebrahemkreem40@gmail.com>
2019-05-27 17:39:36 +0300

    fix #2

--- src/data/FoodComposition/csv_parser.js
+++ src/data/FoodComposition/csv_parser.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 const csv = require('csv-parser')
-const path = require('path');
+const path = require('path')
 
-fs.createReadStream(path.resolve(__dirname + '/Food_Composition.csv'))
+fs.createReadStream(path.join(__dirname, 'Food_Composition.csv'))
   .pipe(csv())
   .on('data', function (data) {
     try {
+      ;
       console.log(data)
     } catch (err) {
       console.log(err)
