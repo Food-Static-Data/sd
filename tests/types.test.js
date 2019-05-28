d3046eda0f121c2d769f9661f793ef0b1930b9d8
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-28 09:59:34 +0100

    Update types.test.js
    issue #22.adding test for types json schema

--- tests/types.test.js
+++ tests/types.test.js
@@ -1,9 +1,27 @@
 /* global describe, it, expect */
 'use strict'
+
+const { matchers }=require('jest-json-schema');
+expect.extend(matchers);
+
 const { types } = require('../files')
 
 describe('types data files returns array', () => {
   it('these tests prevent any issues and problems, also to break the structure of types', () => {
     expect(types).not.toBe('')
   })
+})
+
+describe('types json schema testing', () => {
+    it('validates type json schema', () => {
+      const schema = {
+        properties: {
+          Field: { type: 'string' },
+          Type: { type: 'string' },
+          Description: { type: 'string' }
+        },
+        required: ['Field','Type','Description'],
+      };
+      expect({ Field: 'from' ,Type:'integer',Description:'First result index as submitted'}).toMatchSchema(schema);
+    });
 })
