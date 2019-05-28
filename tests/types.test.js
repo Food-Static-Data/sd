e4e9600a983db1fb03203899246dd39d4a39c6a3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-05-28 12:26:22 +0300

    Update types.test.js

--- tests/types.test.js
+++ tests/types.test.js
@@ -1,11 +1,11 @@
 /* global describe, it, expect */
 'use strict'
 
-const {matchers}=require('jest-json-schema');
+const { matchers } = require('jest-json-schema');
 expect.extend(matchers);
 
-const {typesFile} = require('../files')
-const types=require(typesFile)
+const { typesFile } = require('../files')
+const types = require(typesFile)
 
 describe('types data files returns array', () => {
   it('these tests prevent any issues and problems, also to break the structure of types', () => {
