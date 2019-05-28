c94d32ec8185f7e463db529a056aaa7d26048dff
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-28 10:25:18 +0100

    Update types.test.js

--- tests/types.test.js
+++ tests/types.test.js
@@ -5,7 +5,7 @@ const {matchers}=require('jest-json-schema');
 expect.extend(matchers);
 
 const {typesFile} = require('../files')
-const type=require(typesFile)
+const types=require(typesFile)
 
 describe('types data files returns array', () => {
   it('these tests prevent any issues and problems, also to break the structure of types', () => {
