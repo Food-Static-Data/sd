a4e16a6ad414f744e810653a38ae5301a6f284a6
siddharthp538 <svpandey86@gmail.com>
2019-05-29 21:58:09 +0530

    testing of dash.test.js

--- tests/dash.test.js
+++ tests/dash.test.js
@@ -1,9 +1,30 @@
 /* global describe, it, expect */
 'use strict'
 const staticData = require('../filesObjects')
+const { matchers } = require('jest-json-schema');
+expect.extend(matchers);
+
 
 describe('this test prevents to any issues and problems, also to break the structure of dash data', () => {
   it('dash data files returns object', () => {
     expect(staticData.dash).not.toBe('')
   })
 })
+
+
+describe('test dash test json schema', () => {
+  it('validates my json', () => {
+     const schema = {
+      properties: {
+            name: { type: 'string' },
+            americanStandart: { type: 'string'},
+            americanStandartOunces: {type: 'string' },
+            volume: { type: 'string' },
+            weight: { type: 'string' }
+        },
+      required: [ 'name', 'americanStandart', 'americanStandartOunces','volume','weight' ]  
+    };
+      
+    expect({ name: 'sugar', americanStandart: '2 tablespoons', americanStandartOunces: '1 ounce', volume:'2 milliliters', weight:'100 grams' }).toMatchSchema(schema);
+  });
+});
