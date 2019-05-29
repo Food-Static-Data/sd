3bdde97c566c2fdf3a5cb1607d09c73705015b58
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-29 14:25:31 +0100

    Update pinch.test.js
    issue#22.adding json-schema test for pinch json

--- tests/pinch.test.js
+++ tests/pinch.test.js
@@ -1,9 +1,29 @@
 /* global describe, it, expect */
 'use strict'
+const { matchers } = require('jest-json-schema');
+expect.extend(matchers);
+
 const staticData = require('../filesObjects')
 
 describe('this test prevents to any issues and problems, also to break the structure of pinch data', () => {
   it('pinch data files returns object', () => {
     expect(staticData.pinch).not.toBe('')
   })
+})
+
+describe('testng for pinch json data schema', () => {
+  it('validates pinch json file schema', () => {
+  const schema = {
+    properties: {
+          name: { type: 'string' },
+          americanStandart: { type: 'sting'},
+          americanStandartOunces: {type: 'null' },
+          volume: { type: "null" },
+          weight: { type: 'null' }
+      },
+    required: [ 'name', 'americanStandart', 'americanStandartOunces','volume','weight' ]  
+  };
+    
+  expect({ name: 'teaspoon', americanStandart: '1/16 teaspoon', americanStandartOunces: null, volume: null, weight: null }).toMatchSchema(schema);
+});
 })
