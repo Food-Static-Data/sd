5479d38fd7e36f5776b7cd89e2762fb9f3d6b377
siddharthp538 <svpandey86@gmail.com>
2019-05-29 22:37:03 +0530

    removed yarn

--- tests/dash.test.js
+++ tests/dash.test.js
@@ -1,15 +1,15 @@
 /* global describe, it, expect */
 'use strict'
-const staticData = require('../filesObjects')
+const { dash } = require('../filesObjects')
 const { matchers } = require('jest-json-schema');
 expect.extend(matchers);
 
 
 describe('this test prevents to any issues and problems, also to break the structure of dash data', () => {
   it('dash data files returns object', () => {
-    expect(staticData.dash).not.toBe('')
-  })
-})
+    expect(dash).not.toBe('')
+  });
+});
 
 
 describe('test dash test json schema', () => {
