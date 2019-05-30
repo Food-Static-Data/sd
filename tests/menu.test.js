fa932132af870717809f388c04ba571833f63fd2
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-30 11:02:16 +0100

    Update menu.test.js
    issue#22.adding json-schema test to menu json file

--- tests/menu.test.js
+++ tests/menu.test.js
@@ -1,10 +1,30 @@
 /* global describe, it, expect */
 'use strict'
+const { matchers } = require('jest-json-schema');
+expect.extend(matchers);
+
 const { menuFilePath } = require('../files')
 const menu = require(menuFilePath)
 
 describe('menu data files returns array', () => {
   it('these tests prevent any issues and problems, also to break the structure of menu', () => {
     expect(menu).not.toBe('')
+  })
+})
+
+describe('types json schema testing', () => {
+  it('validates type json schema', () => {
+    const schema = {
+      properties: {
+        id: { type: 'string' },
+        title: { type: 'string' },
+        date: { type: 'string' },
+        description: { type: 'string' },
+        notes: { type: 'string' }
+      },
+      required: ['id', 'title', 'date', 'description', 'notes']
+    }
+    expect({ id: "4abba3e0-6106-11e9-9ffc-a3e52b2d927f", title: "Weekly menu #${index}", date: "2019-04-17T11:45:26.302Z", description: "description for Weekly menu #${index}", notes: "This is a chef notes for wm #${index}"
+}).toMatchSchema(schema)
   })
 })
