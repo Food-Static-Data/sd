81665848095154f7d94526195e76d5c225f06f93
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-30 11:10:30 +0100

    Update menu.test.js

--- tests/menu.test.js
+++ tests/menu.test.js
@@ -24,7 +24,7 @@ describe('types json schema testing', () => {
       },
       required: ['id', 'title', 'date', 'description', 'notes']
     }
-    expect({ id: "4abba3e0-6106-11e9-9ffc-a3e52b2d927f", title: "Weekly menu #${index}", date: "2019-04-17T11:45:26.302Z", description: "description for Weekly menu #${index}", notes: "This is a chef notes for wm #${index}"
+    expect({ id: "4abba3e0-6106-11e9-9ffc-a3e52b2d927f", title: `Weekly menu #${index}`, date: "2019-04-17T11:45:26.302Z", description: `description for Weekly menu #${index}`, notes: `This is a chef notes for wm #${index}`
 }).toMatchSchema(schema)
   })
 })
