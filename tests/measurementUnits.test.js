64e80da8ae409c1711ee86b2de0e9cb64d795e51
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-30 11:45:58 +0100

    Update measurementUnits.test.js
    issue#22.adding json-schema test for measurementunits json in both data/units and data/measurementUnit

--- tests/measurementUnits.test.js
+++ tests/measurementUnits.test.js
@@ -1,5 +1,8 @@
 /* global describe, it, expect */
 'use strict'
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
 const { measurementUnitsFilePath, generatedMeasurementUnits } = require('../files')
 const measurementUnits = require(measurementUnitsFilePath)
 
@@ -12,5 +15,44 @@ describe('users data files returns array', () => {
 describe('users data files returns array', () => {
   it('these tests prevent any issues and problems, also to break the structure of measurementUnits', () => {
     expect(generatedMeasurementUnits).not.toBe('')
+  })
+})
+
+describe(' MeasurementUnits schema testing', () => {
+  it('validates  MeasurementUnits json schema', () => {
+    const schema = {
+    "properties": {
+    "type": { "type": "string" },
+    "name": { "type": "string" },
+    "singular": { "type": "string" },
+    "plural": { "type": "string" },
+    "short": { "type": "string" },
+    "pattern": { "type": "string" }
+  },
+  "required": [ "type", "name", "singular", "plural", "short", "pattern" ]
+
+    expect({"type": "Weight", "name": "Pound", "singular": "pound", "plural": "pounds", "short": "lb", "pattern": "[\\d]{1,2}" }).toMatchSchema(schema)
+  })
+})
+
+describe(' generatedMeasurementUnits schema testing', () => {
+  it('validates  generatedMeasurementUnits json schema', () => {
+    const schema = {
+     "properties": {
+    "id": { "type": "string" },
+    "system_id": { "type": "string" },
+    "type": { "type": "string" },
+    "name": { "type": "string" },
+    "singular": { "type": "string" },
+    "plural": { "type": "string" },
+    "short": { "type": "string" },
+    "pattern": { "type": "string" },
+    "error": { "type": "string" }
+
+  },
+  "required": [ "id", "system_id", "type", "name", "singular", "plural", "short", "pattern", "error" ]
+   
+    }
+    expect({ "id": "fbe54b40-7a3e-11e9-8ec5-0d5ec94f7bcf", "system_id": "fbe57235-7a3e-11e9-8ec5-0d5ec94f7bcf", "type": "Weight", "name": "Pound", "singular": "pound", "plural": "pounds", "short": "lb", "pattern": "[\\d]{1,2}", "error": "null"}).toMatchSchema(schema)
   })
 })
