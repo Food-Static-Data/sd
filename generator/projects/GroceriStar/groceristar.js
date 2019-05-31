8d5509fa83d8941751cb1e835491cfb2125d92cc
Edebo <36434800+Edebo@users.noreply.github.com>
2019-05-31 14:25:11 +0100

    Update groceristar.js
    issue #76 : adding methods to generate file like https://github.com/GroceriStar/sd/blob/master/src/data/Grocery/example/ultimate-gl-from-loopback-server-example.json

--- generator/projects/GroceriStar/groceristar.js
+++ generator/projects/GroceriStar/groceristar.js
@@ -1,7 +1,7 @@
 const _ = require('lodash')
 const { __generateId } = require('../../../src/utils')
 
-const { departments, ingredients } = require('../../../files')
+const { departments, ingredients, grocery } = require('../../../files')
 /**
  * @returns {array} of keys for departments and ingredients
  */
@@ -26,6 +26,77 @@ const getKeyArrayDepAndIng = () => {
   return keys
 }
 
+
+
+//get ultimate grocery list for each grocery store
+const ultimateGroceryList=()=>{
+  const ultimategroceries=[]
+  const groceries=getAllGroceryWithId();
+
+  _.map(groceries,grocery=>{
+    const ultimategrocery={}
+    
+    ultimategrocery.name=grocery.name;
+    ultimategrocery.groceryId=grocery.key;
+    ultimategroceries.messages={};
+    ultimategrocery.departments=getGroceryDepartmentsWithIngredients(grocery.departments,grocery.key);
+    ultimategrocery.ultimate={name:grocery.name, id:grocery.key}
+    
+    ultimategroceries.push(ultimategrocery)
+  })  
+
+  return ultimategroceries
+}
+
+//get all departments with their ingredients in a grocery
+const getGroceryDepartmentsWithIngredients=(grocerydept,key)=>{
+  const departs=[]
+  let departments = getAllDepartmentsWithId()
+  _.map(grocerydept,dept=>{
+    //search for a particular grocery department in the department json to get the obj
+    const deptObj= departments.filter(depart=>{
+        return depart.name === dept;
+      })
+
+      if (deptObj){
+        const obj={ id: deptObj.key, name: deptObj.name, type: deptObj.type}
+
+        obj.ingredients=getDepartmentIngredients(dept,key) //add all the ingredients in this department to the obj
+        departs.push(deptObj)
+      }     
+    })
+   
+    return departs;
+  }
+
+
+//get all ingredients in a department
+const getDepartmentIngredients=(department,key)=>{
+  const ingredients=[];
+  let ingreds = getAllIngredientsWithId()
+  _.map(ingreds,ingredient=>{
+      if(ingredient.department === department){
+        const ingred=[
+          ingredient.key,
+          ingredient.name,
+          `/del/ing/${ingredient.key}/${key}`
+        ]
+        ingredients.push(ingred);
+      }
+
+  })
+
+  return ingredients;
+}
+
+
+//get grocery with key
+const getAllGroceryWithId= () => {
+  let result = getResult(grocery)
+
+  return result
+}
+
 const getAllDepartmentsWithId = () => {
   let result = getResult(departments)
 
@@ -43,4 +114,4 @@ const getResult = (property) => _.map(property, (p) => ({
   ...p
 }))
 
-module.exports = { getKeyArrayDepAndIng }
+module.exports = { getKeyArrayDepAndIng, ultimateGroceryList }
