92279769854cb555097953450ccd9b6b2d7746d7
Prashant Uniyal <36094606+uniyalprashant9@users.noreply.github.com>
2019-06-01 23:22:56 +0530

    Gives content of files in element

---
+++ src/indexContent.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+var src = ".././src/data/Grocery/elements/";
+var content = [];
+
+var files = fs.readdirSync(src);
+
+files.forEach(file => {
+    let fileStat = fs.statSync(src + file).isDirectory();
+    if (!fileStat) {
+        let data = fs.readFileSync(src + file);
+        data = JSON.parse(data);
+        content.push(data);
+    }
+});
+
+console.log(content);
+
+module.exports = content;
