b530d00bcd023ab190de68c96e5d5ff55e39dc6a
Edebo <36434800+Edebo@users.noreply.github.com>
2019-06-01 12:00:13 +0100

    Update allergy.test.js
    issue #22,adding json-schema test to allergy json file

--- tests/allergy.test.js
+++ tests/allergy.test.js
@@ -1,10 +1,29 @@
 /* global describe, it, expect */
 'use strict'
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
 const staticData = require('../filesObjects')
 // const allergies = require(allergiesFilePath)
 
 describe('tests for allergy', () => {
   it('allergies data files returns array', () => {
     expect(staticData.allergies).not.toBe('')
+  })
+})
+const schema={
+  type: 'array', items: [{ type: 'string'}]
+}
+
+const example=[
+  "Gluten-Free",
+  "Peanut-Free",
+  "Seafood-Free",
+  "Sesame-Free"
+]
+
+describe('tests for allergy schema', () => {
+  it('validates allergy json', () => {
+    expect(example).toMatchSchema(schema);
   })
 })
