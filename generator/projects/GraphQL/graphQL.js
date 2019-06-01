6e04e9ac32a673bbfea7fe179a01536ab019d988
Edebo <36434800+Edebo@users.noreply.github.com>
2019-06-01 18:18:06 +0100

    Update graphQL.js
    working on the todos in https://github.com/GroceriStar/sd/edit/master/generator/projects/GraphQL/graphQL.js

--- generator/projects/GraphQL/graphQL.js
+++ generator/projects/GraphQL/graphQL.js
@@ -15,7 +15,7 @@ const getFavoritesKey = function () {
 const getDepartmentsKey = function () {
   let results = departments
   return results.map((item, index) => ({
-    department_id: __generateId(),
+    departmentId: __generateId(),
     name: item.name,
     desc: 'description for department1',
     created_at: __generateDate(),
@@ -41,19 +41,19 @@ const getItemsKey = function () {
   })
 }
 
-// @TODO don't like that ingredient and grocery ids...
+
 const getUsersKey = function () {
   let results = users
 
   return results.map((item, index) => ({
     userId: __generateId(),
     favs: false,
-    ingredient_id: 1,
-    grocery_id: 1
+    ingredientId: 1,
+    groceryId: 1
   }))
 }
 
-// @TODO don't like that id_1 and department ids...
+
 const getIngredientsKey = function (limit = false) {
   let results = ingredients
 
@@ -62,31 +62,31 @@ const getIngredientsKey = function (limit = false) {
   }
 
   return results.map((item, index) => ({
-    ingredient_id: __generateId(),
+    ingredientId: __generateId(),
     favs: '',
     name: item.name,
     description: 'description',
     custom: false,
     created_at: __generateDate(),
     updated_at: __generateDate(),
-    id_1: 1,
+    id: 1,
     department_id: 1
   }))
 }
 
-// @TODO id_1
+
 const getGroceryKey = function () {
   let results = grocery
 
   return results.map((item, index) => ({
-    grocery_id: __generateId(),
+    groceryId: __generateId(),
     name: item.name,
     img: item.img,
     desc: item.desc,
     slug: item.slug,
     created_at: __generateDate(),
     updated_at: __generateDate(),
-    id_1: 1,
+    id: 1,
     favs: false
   }))
 }
