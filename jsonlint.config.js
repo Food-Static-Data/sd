04986c94c345965a663d52ea5ceb1a169e3f1bad
Edebo <36434800+Edebo@users.noreply.github.com>
2019-06-01 19:30:56 +0100

    Update jsonlint.config.js
    working on the todos of jsonlint.config.js.i have figure out what needs to be done in the first todo

--- jsonlint.config.js
+++ jsonlint.config.js
@@ -4,13 +4,18 @@ var shell = require('shelljs')
 // @TODO add here all new generated at generator files too
 const jsonFiles = require('./files.js')
 
-// @TODO we can replace this loops with lodash simple versions.
-Object.keys(jsonFiles).forEach(function (key) {
-  shell.exec('jsonlint ' + jsonFiles[key], { silent: true })
+//each can be used instead of forEach
+_.forEach( Object.keys(jsonFiles), function (key) {
+    try{
+      
+      shell.exec('jsonlint ' + jsonFiles[key], { silent: true })
+    }
+    catch(e){
+      
+       console.log(e)
+    }
 
-  // @TODO we need to add some validation, that will help us to know, when something is wrong.
-  // how to test it - we can break one file and when it go to if statement - we'll be able to
-  // see is it works or not
+ 
 
   // // Run external tool synchronously
   // if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
