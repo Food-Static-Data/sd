18cbc20d6f904a6800cdf333f6f31996cc036cf4
Prashant Uniyal <36094606+uniyalprashant9@users.noreply.github.com>
2019-06-02 10:30:31 +0530

    New index module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
-var src = ".././src/data/Grocery/elements/";
-var list = [];
-var files = fs.readdirSync(src);
 
-files.forEach(file => {
-    let fileStat = fs.statSync(src + file).isDirectory();
-    if (!fileStat) {
-        list.push(file);
-    }
-});
-console.log(list);
-module.exports = list;
+function index() {
+
+    var src = ".././src/data/Grocery/elements/";
+    var list = [];
+    var files = fs.readdirSync(src);
+
+    files.forEach(file => {
+        let fileStat = fs.statSync(src + file).isDirectory();
+        if (!fileStat) {
+            list.push(file);
+        }
+    });
+    console.log(list);
+    module.exports = list;
+}
+index();
--- src/indexContent.js
+++ src/indexContent.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
-var src = ".././src/data/Grocery/elements/";
-var content = [];
 
-var files = fs.readdirSync(src);
+function indexContent() {
+    var src = ".././src/data/Grocery/elements/";
+    var content = [];
+    var files = fs.readdirSync(src);
+    files.forEach(file => {
+        let fileStat = fs.statSync(src + file).isDirectory();
+        if (!fileStat) {
+            let data = fs.readFileSync(src + file);
+            data = JSON.parse(data);
+            content.push(data);
+        }
+    });
+    console.log(content);
+    module.exports = content;
+}
 
-files.forEach(file => {
-    let fileStat = fs.statSync(src + file).isDirectory();
-    if (!fileStat) {
-        let data = fs.readFileSync(src + file);
-        data = JSON.parse(data);
-        content.push(data);
-    }
-});
-
-console.log(content);
-
-module.exports = content;
+indexContent();
