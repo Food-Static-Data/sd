713f4504247bc82e4132fd1707bb295ef69837e5
Prashant Uniyal <uniyalprashant9@gmail.com>
2019-06-03 23:00:45 +0530

    merged splitObject.js

--- generator/writeFile.js
+++ generator/writeFile.js
@@ -51,7 +51,42 @@ function test () {
 // execute function
 // writeFiles()
 
+
+
+
+/*
+This module will create a directory with the name of object file 
+and puts all the splitted elements inside the directory.
+*/
+const fs = require('fs');
+function splitObject() {
+    var src = ".././src/data/Grocery/"; // Location where all object files are save
+    var obj = "grocery.json"; //Object file name 
+    var path = src + obj; // Path of object file
+    let data = fs.readFileSync(path);
+    let grocery = JSON.parse(data);
+    var len = grocery.length; //Object Length
+
+    var foldername = 'elements';
+    if (!fs.existsSync(src + foldername)) { //Check if Folder exists or not.
+        fs.mkdirSync(src + foldername);
+    }
+    for (var i = 0; i < len; i++) {
+        let temp = JSON.stringify(grocery[i]);
+        var fileName = grocery[i].name + ".json";
+        fileName = fileName.replace(/ /g, "_"); //Replace space with underscore 
+        fileName = fileName.toLowerCase();
+        //Checking if file already exists or not
+        if (!fs.existsSync(src + foldername + "/" + fileName)) {
+            fs.writeFileSync(src + foldername + "/" + fileName, temp);
+        } else {
+            console.log("File <" + fileName + "> Already Exists.");
+        }
+    }
+}
+
 module.exports = {
   writeFile,
-  test
+  test,
+  splitObject
 }
