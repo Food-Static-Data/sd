8b3b6daf94d8805594f3951d4626a2d64324f9f5
siddharthp538 <svpandey86@gmail.com>
2019-06-04 00:07:05 +0530

    testing done for few files

--- tests/cuisine.test.js
+++ tests/cuisine.test.js
@@ -3,8 +3,50 @@
 const { cuisineFilePath } = require('../files')
 const cuisines = require(cuisineFilePath)
 
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
 describe('this test prevents to any issues and problems, also to break the structure of cuisines data files', () => {
   it('cuisines data files returns array', () => {
     expect(cuisines).not.toBe('')
+  })
+})
+
+const schema = {
+  type: 'array', items: [{ type: 'string' }]
+}
+
+const example = [
+  'American',
+  'Kid-Friendly',
+  'Italian',
+  'Asian',
+  'Mexican',
+  'Southern & Soul Food',
+  'French',
+  'Southwestern',
+  'Barbecue',
+  'Indian',
+  'Chinese',
+  'Cajun & Creole',
+  'Mediterranean',
+  'Greek',
+  'English',
+  'Spanish',
+  'Thai',
+  'German',
+  'Moroccan',
+  'Irish',
+  'Japanese',
+  'Cuban',
+  'Hawaiian',
+  'Swedish',
+  'Hungarian',
+  'Portuguese'
+]
+
+describe('tests for cuisine schema', () => {
+  it('validates cuisine json', () => {
+    expect(example).toMatchSchema(schema)
   })
 })
--- tests/departments.test.js
+++ tests/departments.test.js
@@ -3,8 +3,190 @@
 const { departmentsFilePath } = require('../files')
 const departments = require(departmentsFilePath)
 
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
 describe('departments data files returns array', () => {
   it('these tests prevent any issues and problems, also to break the structure of departments', () => {
     expect(departments).not.toBe('')
+  })
+})
+
+const schema = {
+  properties: {
+    name: {
+      type: 'string'
+    },
+    type: {
+      type: 'string'
+    }
+
+  },
+  required: ['name', 'type']
+}
+let example = {
+  'name': 'Fresh vegetables',
+  'type': 'food'
+}
+
+describe('test fresh vegetable  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+example = {
+  'name': 'Condiments / Sauces',
+  'type': 'food'
+}
+describe('test condiments/sauces  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Dairy',
+  'type': 'food'
+}
+describe('test dairy  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Cheese',
+  'type': 'food'
+}
+describe('test cheese  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Meat',
+  'type': 'food'
+}
+
+describe('test meat  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Seafood',
+  'type': 'food'
+}
+describe('test seafood  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Beverages',
+  'type': 'food'
+}
+
+describe('test beverages  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Baked goods',
+  'type': 'food'
+}
+
+describe('test baked goods  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Baking',
+  'type': 'food'
+}
+
+describe('test baking json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Snacks',
+  'type': 'food'
+}
+
+describe('test snacks json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Themed meals',
+  'type': 'food'
+}
+describe('test themed meals json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Baby stuff',
+  'type': 'non-food'
+}
+describe('test baby stuff  json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Pets',
+  'type': 'non-food'
+}
+describe('test pets json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Fresh fruits',
+  'type': 'food'
+}
+describe('test fresh fruits json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Refrigerated items',
+  'type': 'food'
+}
+
+describe('test refrigerated items json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
+  })
+})
+
+example = {
+  'name': 'Frozen',
+  'type': 'food'
+}
+
+describe('test frozen json schema', () => {
+  it('validates my json', () => {
+    expect(example).toMatchSchema(schema)
   })
 })
