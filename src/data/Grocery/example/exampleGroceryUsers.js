01e8c3ba380d65b1b7be01e6cae79610d5161e48
bruno <brunocordeiro180@gmail.com>
2019-06-03 21:36:17 -0300

    Split src/Grocery/example/example.js in two files

---
+++ src/data/Grocery/example/exampleGroceryUsers.js
@@ -0,0 +1,10 @@
+const groceryUsers  = require('./grocery-with-user-relations.json')
+
+// console.log('Name of User: ' + groceryUsers['name'])
+// console.log(ultimateGl["departments"][0]);
+
+
+module.exports = {
+  groceryUsers
+}
+
---
+++ src/data/Grocery/example/exampleUltimateGl.js
@@ -0,0 +1,9 @@
+const ultimateGl = require('./ultimate-gl-from-loopback-server-example.json')
+
+// for (let i = 0; i < ultimateGl['departments'].length; i++) {
+//     console.log(ultimateGl['departments'][i]['name'])
+//   }
+
+module.exports = {
+    ultimateGl
+}
