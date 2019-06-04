a701d9b13f8f5a8d272d5833325273922a60c31f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-04 04:27:39 +0300

    Update exampleGroceryUsers.js

--- src/data/Grocery/example/exampleGroceryUsers.js
+++ src/data/Grocery/example/exampleGroceryUsers.js
@@ -1,10 +1,10 @@
-const groceryUsers  = require('./grocery-with-user-relations.json')
+const groceryListsWithUsersRelations  = require('./grocery-with-user-relations.json')
 
-// console.log('Name of User: ' + groceryUsers['name'])
-// console.log(ultimateGl["departments"][0]);
+// console.log('Name of User: ' + groceryListsWithUsersRelations['name'])
+// console.log(groceryListsWithUsersRelations["departments"][0]);
 
 
 module.exports = {
-  groceryUsers
+  groceryListsWithUsersRelations
 }
 
