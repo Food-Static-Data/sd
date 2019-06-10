e5a8bdd92a92c35c2c0f1cfea4e157a4f78b1316
Andrew Serra <andy@serra.us>
2019-06-10 22:24:46 +0300

    added files to check if the files under tests/examples/ exist. only added a check for examples/allergy.js

---
+++ tests/testFilesCheck/exampleFiles.test.js
@@ -0,0 +1,9 @@
+const exampleFiles = require('./exampleFilesNames')
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
+describe('tests for example file existance', () => {
+  it('validates that allergy example file exists', () => {
+    expect(exampleFiles.allergy).not.toBeUndefined()
+  })
+})
---
+++ tests/testFilesCheck/exampleFilesNames.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const fs = require('fs')
+//const _ = require('lodash')
+
+const directoryPath = path.join(__dirname, '/../examples/')
+
+let exampleFiles = {};
+
+fs.readdirSync(directoryPath).forEach((file) => {
+  if(path.extname(file) === '.js') {
+    let filename = path.basename(file, path.extname(file));
+    exampleFiles[filename] = path.join(directoryPath, file);
+  }
+});
+
+module.exports = exampleFiles
