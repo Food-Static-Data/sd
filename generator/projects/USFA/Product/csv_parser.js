ee67855ef522585c73978b41e892fa8cc23f81af
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-15 07:16:34 +0300

    Update csv_parser.js

--- generator/projects/USFA/Product/csv_parser.js
+++ generator/projects/USFA/Product/csv_parser.js
@@ -13,9 +13,18 @@ const headers = [
   'ingredients_english'
 ]
 
+//@TODO I don't like this dir variable
+
 const dir = '../../../src/data/Product'
+
 // //joining path of directory
 const directoryPath = path.join(__dirname, dir)
+
+// @TODO this function is a duplicate. Create a method at csv_parser and import it here
+
+// @TODO change the name of this file - it's confusing, because we have a same name imported.
+// let it be just parser.
+
 // passing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
   // handling error
