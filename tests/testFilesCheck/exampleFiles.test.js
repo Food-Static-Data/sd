ca5aee40dede86a15b918e6419ef8b4ca5853d81
Amogh J <amoghjahagirdargsf@gmail.com>
2019-06-16 10:48:35 +0400

    upgrading exampleFiles.test.js

--- tests/testFilesCheck/exampleFiles.test.js
+++ tests/testFilesCheck/exampleFiles.test.js
@@ -1,11 +1,13 @@
 /* global describe, it, expect */
 const exampleFiles = require('./exampleFilesNames')
 const { matchers } = require('jest-json-schema')
+const {} = require('./examples')
 expect.extend(matchers)
 
-describe('tests for example file existance', (type) => {
 
-  it(`validates that ${type} example file exists`, () => {
-    expect(exampleFiles.type).not.toBeUndefined()
-  })
+describe('tests for example file existance', () => {
+  const doesFileExist = (type) =>{
+  test(`validates that ${type} example file exists`, () => {
+    expect(exampleFiles[type]).not.toBeUndefined()
+  })}
 })
