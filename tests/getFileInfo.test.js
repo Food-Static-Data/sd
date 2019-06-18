59f40e1120243a86a8a4f4f19ebb6c2e2ab0567b
Prashant Uniyal <uniyalprashant9@gmail.com>
2019-06-18 21:03:42 +0530

    tests for getFileInfo
    path fix

--- tests/getFileInfo.test.js
+++ tests/getFileInfo.test.js
@@ -5,7 +5,7 @@ const { matchers } = require('jest-json-schema')
 expect.extend(matchers)
 describe('getFileInfo', () => {
     it('should return the list of files in the folder', () => {
-        const result = utils.getFileInfo('./sampleFile')
+        const result = utils.getFileInfo('/home/travis/build/GroceriStar/sd/test/sampleFile')
         const expected = ['basic-grocery-list.json']
         expect(result.values).toEqual(expected.values)
     })
@@ -13,7 +13,7 @@ describe('getFileInfo', () => {
 
 describe('getFileInfo', () => {
     it('should return the content of given file', () => {
-        const result = utils.getFileInfo('./sampleFile', 1, 'basic-grocery-list.json')
+        const result = utils.getFileInfo('/home/travis/build/GroceriStar/sd/test/sampleFile', 1, 'basic-grocery-list.json')
         const expected = { "departments": ["staples", "tinnedFood", "vegetables"], "name": "basic-grocery-list", "img": false, "desc": false, "slug": false }
         expect(result.values).toEqual(expected.values)
     })
