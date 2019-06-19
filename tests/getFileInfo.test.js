231d542ca2146ebb52005aae004145b475b1abe8
Prashant Uniyal <uniyalprashant9@gmail.com>
2019-06-19 20:21:32 +0530

    Update getFileInfo.test.js

--- tests/getFileInfo.test.js
+++ tests/getFileInfo.test.js
@@ -5,7 +5,7 @@ const { matchers } = require('jest-json-schema')
 expect.extend(matchers)
 describe('getFileInfo with only 1 argument', () => {
     it('should return the list of files in the folder', () => {
-        const result = getFileInfo('./../tests/sampleFile')
+        const result = getFileInfo('./sampleFile')
         const expected = ['basic-grocery-list.json']
         expect(result.values).toEqual(expected.values)
     })
@@ -13,7 +13,7 @@ describe('getFileInfo with only 1 argument', () => {
 
 describe('getFileInfo with all 3 arguments', () => {
     it('should return the content of given file', () => {
-        const result = getFileInfo('./../tests/sampleFile', 1, 'basic-grocery-list.json')
+        const result = getFileInfo('./sampleFile', 1, 'basic-grocery-list.json')
         const expected = { "departments": ["staples", "tinnedFood", "vegetables"], "name": "basic-grocery-list", "img": false, "desc": false, "slug": false }
         expect(result.values).toEqual(expected.values)
     })
