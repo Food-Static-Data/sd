c7967796ab79b51f588c910cbad6cbe14d5dd76b
Edebo <36434800+Edebo@users.noreply.github.com>
2019-06-19 01:11:19 +0100

    Update and rename csv_parser.js to parser.js
    #471 comments

---
+++ generator/projects/USFA/Product/parser.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const { parseDirectoryFiles } = require('../../../csv_parser')
+
+const headers = [
+  'NDB_Number',
+  'long_name',
+  'data_source',
+  'gtin_upc',
+  'manufacturer',
+  'date_modified',
+  'date_available',
+  'ingredients_english'
+]
+
+const dir = '../../../../src/data/Product'
+// //joining path of directory
+const directoryPath = path.join(__dirname, dir)
+
+parserFiles(directoryPath,headers)
