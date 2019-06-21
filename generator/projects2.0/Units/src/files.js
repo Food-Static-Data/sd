6eeb280b7868c2517a0a8e281296ab1ce2dbcd35
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-22 00:39:35 +0300

    units with fixes

--- generator/projects2.0/Units/src/files.js
+++ generator/projects2.0/Units/src/files.js
@@ -1,13 +1,35 @@
+// here we'll connect files, that will help us to manage units.
+// it's partially similar to Measurements, but our goal here will be a bit different
 import {
-  departments,
-  groceries,
-  ingredients,
-  users
+  cookingTimes,
+  cup1,
+  halfCup,
+  quaterCup,
+  cup18,
+  cup34, //@TODO replace this names aka cup 1/8 with text version for better understanding
+  dash,
+  gallon,
+  pinch,
+  pint,
+  quart,
+  tablespoon,
+  teaspoon,
+  types //@TODO do we need that types?
 } from '@groceristar/sd-wrapper'
 
 export {
-  departments,
-  groceries,
-  ingredients,
-  users
+  cookingTimes,
+  cup1,
+  halfCup,
+  quaterCup,
+  cup18,
+  cup34,
+  dash,
+  gallon,
+  pinch,
+  pint,
+  quart,
+  tablespoon,
+  teaspoon,
+  types
 }
