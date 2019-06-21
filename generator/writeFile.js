29aa07cbcdc832e31ecfaaa307e05991190ef882
Prashant Uniyal <uniyalprashant9@gmail.com>
2019-06-21 07:19:23 +0530

    Update writeFile.js
    `@TODO do we have only one this method or i'm mistaken?`
    -->  yes we have only one method.
    
    the whitespaces are auto added from the vscode editor (extension).

--- generator/writeFile.js
+++ generator/writeFile.js
@@ -3,19 +3,19 @@
 const fs = require('fs')
 const PATH = require('path')
 const { promisify } = require('util') // ?? it's utils of not
-// const { promisify } = require('util')
+    // const { promisify } = require('util')
 
 // const _ = require('lodash')
 
 const writeFilePromisify = promisify(fs.writeFile)
 
 // @TODO change the name of this method. it's not selfexplanatory
-async function main (path, data) {
-  await writeFilePromisify(
-    path, data
-  )
+async function main(path, data) {
+    await writeFilePromisify(
+        path, data
+    )
 
-  console.info('file generated successfully!')
+    console.info('file generated successfully!')
 }
 
 /**
@@ -23,39 +23,49 @@ async function main (path, data) {
  * @param {String} path
  * @param {Object} data
  */
-function writeFile (path, data) { // @TODO looks like a duplicate, am i right?
-  // console.log(typeof users);
-  // console.log(typeof usersStr);
-  if (typeof data === 'undefined') {
-    console.error('Error variable is undefined')
-    return
-  }
-
-  if (typeof data === 'object') {
-    var dataStr = JSON.stringify(data)
-    if (typeof dataStr !== 'string') {
-      console.error('Error occured after stringify or variabe has another type not string')
-      return
+function writeFile(path, data) { // @TODO looks like a duplicate, am i right?
+    // console.log(typeof users);
+    // console.log(typeof usersStr);
+    if (typeof data === 'undefined') {
+        console.error('Error variable is undefined')
+        return
     }
 
-    main(path, dataStr).catch(
-      error => console.error(error)
-    )
-  }
+    if (typeof data === 'object') {
+        var dataStr = JSON.stringify(data)
+        if (typeof dataStr !== 'string') {
+            console.error('Error occured after stringify or variabe has another type not string')
+            return
+        }
+
+        main(path, dataStr).catch(
+            error => console.error(error)
+        )
+    }
 }
 
-function test () {
-  // console.log(filePath["groceryFilePath"]);
-  // console.log(JSON.parse("src/data/Grocery/grocery.json"));
-  // console.log(grocery);
-  // writeFiles()
-  // console.log(typeof require(filePath.groceryFilePath));
+function test() {
+    // console.log(filePath["groceryFilePath"]);
+    // console.log(JSON.parse("src/data/Grocery/grocery.json"));
+    // console.log(grocery);
+    // writeFiles()
+    // console.log(typeof require(filePath.groceryFilePath));
 
-  console.log('ok')
+    console.log('ok')
 }
 
 // execute function
 // writeFiles()
+
+/**
+ * For fixPath()
+ * @param {String} path
+ */
+function fixPath(path) {
+    path = PATH.resolve(__dirname, path) //absolute path
+    if (path[-1] !== '/') { path = path + '/' } //path correction
+    return path
+}
 
 /**
  * For splitObject
@@ -63,30 +73,30 @@ function test () {
  * @param {String} file
  * @param {var} flag
  */
-function splitObject (path, file, flag) { // @TODO do we have only one this method or i'm mistaken?
-  /*
-    flag=1 ==> name according to index
-    flag=0 ==> name according to "name" attribute
-  */
-  path = PATH.resolve(__dirname,path) //path correction
-  if (path[-1] !== '/') { path = path + '/' }
+function splitObject(path, file, flag) { // split large files into single elements
+    /*
+      flag=1 ==> name according to index
+      flag=0 ==> name according to "name" attribute
+    */
+    path = fixPath(path)
 
-  // Reading data...
-  let data = fs.readFileSync(path + file)
-  let fileData = JSON.parse(data)
+    // Reading data...
+    let data = fs.readFileSync(path + file)
+    let fileData = JSON.parse(data)
 
-  var folderName = file.slice(0, -5) + '_elements'
-  var folderNamePath = path + folderName
-
-  if (isDirectory(folderNamePath)) {
-    fs.mkdirSync(folderNamePath)
-  }
-
-  for (var i = 0; i < fileData.length; i++) {
-    var fileName = getFileName(file, fileData[i], flag, i)
-    var elementPath = path + folderName + '/' + fileName
-    writeFile(elementPath, fileData[i])
-  }
+    //new folder to save splitted files
+    var folderName = file.slice(0, -5) + '_elements'
+    var folderNamePath = path + folderName
+    if (isDirectory(folderNamePath)) {
+        fs.mkdirSync(folderNamePath)
+    }
+    //saving files
+    var fileDataLength = fileData.length
+    for (var i = 0; i < fileDataLength; i++) {
+        var fileName = getFileName(file, fileData[i], flag, i)
+        var elementPath = folderNamePath + '/' + fileName
+        writeFile(elementPath, fileData[i])
+    }
 }
 // execute function
 // splitObject()
@@ -95,21 +105,21 @@ function splitObject (path, file, flag) { // @TODO do we have only one this meth
  * fixFileName()
  * @param {string} fileName
  */
-function fixFileName (fileName) {
-  fileName = fileName.replace(/ /g, '_') // Replace space with underscore
-  fileName = fileName.toLowerCase() // Maintain Uniformity
-  return fileName
+function fixFileName(fileName) {
+    fileName = fileName.replace(/ /g, '_') // Replace space with underscore
+    fileName = fileName.toLowerCase() // Maintain Uniformity
+    return fileName
 }
 
-/** isDirectory()
+/**
+ * isDirectory()
  * @param {string} folderNamePath
  *  */
-function isDirectory (folderNamePath) {
-  if (fs.existsSync(folderNamePath)) {
-    return false
-  } else {
+function isDirectory(folderNamePath) {
+    if (fs.existsSync(folderNamePath)) {
+        return false
+    }
     return true
-  }
 }
 
 /**
@@ -118,18 +128,18 @@ function isDirectory (folderNamePath) {
  * @param {Object} fileData
  * @param {var} flag
  * @param {var} index
-*/
-function getFileName (file, fileData, flag, index) {
-  var fileName
-  if (flag === 1) fileName = index + '-' + file// for example: 23-someJsonFile.json
-  else fileName = fileData.name + '.json' // for example: someValueOfName.json
+ */
+function getFileName(file, fileData, flag, index) {
+    var fileName
+    if (flag === 1) fileName = index + '-' + file // for example: 23-someJsonFile.json
+    else fileName = fileData.name + '.json' // for example: someValueOfName.json
 
-  fileName = fixFileName(fileName)
-  return fileName
+    fileName = fixFileName(fileName)
+    return fileName
 }
 
 module.exports = {
-  writeFile,
-  test,
-  splitObject
+    writeFile,
+    test,
+    splitObject
 }
