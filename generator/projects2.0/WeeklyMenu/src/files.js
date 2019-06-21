a6d6b95226c21a3876dd93aeb37267c165d6f057
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-22 00:29:03 +0300

    weekly menu

---
+++ generator/projects2.0/WeeklyMenu/src/files.js
@@ -0,0 +1,7 @@
+import {
+  menu
+} from '@groceristar/sd-wrapper'
+
+export {
+  menu
+}
---
+++ generator/projects2.0/WeeklyMenu/src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash'
+
+import {
+  menu
+} from '@files'
+
+import { __generateId, __generateDate } from '@utils'
+
+export {
+
+}
