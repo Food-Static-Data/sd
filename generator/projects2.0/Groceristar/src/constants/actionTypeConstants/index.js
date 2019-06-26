f2fa81cb8b58d90380a9f6c307b8647bfe4f690b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-27 01:44:09 +0300

    samples

--- generator/projects2.0/Groceristar/src/constants/actionTypeConstants/index.js
+++ generator/projects2.0/Groceristar/src/constants/actionTypeConstants/index.js
@@ -1,3 +1,29 @@
 // is it simple export of we should here keep some methods?
 // like func where we connect files with models
 // aka get(filename) switch case GS_DEPARTMENTS return @files/departments...
+import {
+  GS_DEPARTMENTS, GS_GROCERY, GS_INGREDIENTS, GS_USERS
+} from './models'
+
+
+export const selectSubreddit = subreddit => ({
+  type: SELECT_SUBREDDIT,
+  subreddit
+})
+
+export const invalidateSubreddit = subreddit => ({
+  type: INVALIDATE_SUBREDDIT,
+  subreddit
+})
+
+export const requestPosts = subreddit => ({
+  type: REQUEST_POSTS,
+  subreddit
+})
+
+export const receivePosts = (subreddit, json) => ({
+  type: RECEIVE_POSTS,
+  subreddit,
+  posts: json.data.children.map(child => child.data),
+  receivedAt: Date.now()
+})
--- generator/projects2.0/Measurements/src/constants/actionTypeConstants/index.js
+++ generator/projects2.0/Measurements/src/constants/actionTypeConstants/index.js
@@ -8,3 +8,26 @@ export const GET_MEASUREMENTS = 'GET_MEASUREMENTS'
 export const GET_MEASUREMENT_SYSTEM = 'GET_MEASUREMENT_SYSTEM'
 export const SHOW_ALL_MEASUREMENTS = 'SHOW_ALL_MEASUREMENTS'
 export const FILTER_ITEMS = 'FILTER_ITEMS'
+
+
+export const selectSubreddit = subreddit => ({
+  type: SELECT_SUBREDDIT,
+  subreddit
+})
+
+export const invalidateSubreddit = subreddit => ({
+  type: INVALIDATE_SUBREDDIT,
+  subreddit
+})
+
+export const requestPosts = subreddit => ({
+  type: REQUEST_POSTS,
+  subreddit
+})
+
+export const receivePosts = (subreddit, json) => ({
+  type: RECEIVE_POSTS,
+  subreddit,
+  posts: json.data.children.map(child => child.data),
+  receivedAt: Date.now()
+})
