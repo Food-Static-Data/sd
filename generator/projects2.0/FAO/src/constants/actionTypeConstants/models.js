65ae405f8ddfd81ea6b12e7eb5f47780d6f66bd0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-28 23:23:10 +0300

    fao constants as separated and important commit

--- generator/projects2.0/FAO/src/constants/actionTypeConstants/models.js
+++ generator/projects2.0/FAO/src/constants/actionTypeConstants/models.js
@@ -1,4 +1,38 @@
-// export const FAO_DEPARTMENTS = 'GS_DEPARTMENTS'
-// export const FAO_GROCERY = 'GS_GROCERY'
-// export const FAO_INGREDIENTS = 'GS_INGREDIENTS'
-// export const FAO_USERS = 'GS_USERS'
+export const FAO_FOOD_FILE = 'FAO_FOOD_FILE'
+export const FAO_FISH_FILE = 'FAO_FISH_FILE'
+export const FAO_PULSES_FILE = 'FAO_PULSES_FILE'
+
+export const GET_FOOD_BY_TYPE = 'GET_FOOD_BY_TYPE'
+export const GET_FISH_BY_TYPE = 'GET_FISH_BY_TYPE'
+export const GET_PULSES_BY_TYPE = 'GET_PULSES_BY_TYPE'
+
+export const GET_FOOD_BY_ID = 'GET_FOOD_BY_ID'
+export const GET_FISH_BY_ID = 'GET_FISH_BY_ID'
+export const GET_PULSES_BY_ID = 'GET_PULSES_BY_ID'
+
+export const GET_FULL_DATA = 'GET_FULL_DATA'
+export const GET_SHORT_DATA = 'GET_SHORT_DATA'
+
+function paginate = (data, limit) => {}
+
+//@TODO can be updated with structure like
+// {
+//  file: FAO_FOOD_FILE, number: number
+// }
+const getRandom = (file, number) => {
+  switch (file) {
+    case FAO_FOOD_FILE: {
+
+    }
+    case FAO_FISH_FILE: {
+
+    }
+    case FAO_PULSES_FILE: {
+
+    }
+
+
+    default:
+
+  }
+}
