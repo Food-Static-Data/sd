5dd1c0bdc1f41615b8792d974f9333f935ee190f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-28 23:34:08 +0300

    groceristar as a separated and important commit

--- generator/projects2.0/Groceristar/src/constants/actionTypeConstants/models.js
+++ generator/projects2.0/Groceristar/src/constants/actionTypeConstants/models.js
@@ -2,3 +2,36 @@ export const GS_DEPARTMENTS = 'GS_DEPARTMENTS'
 export const GS_GROCERY = 'GS_GROCERY'
 export const GS_INGREDIENTS = 'GS_INGREDIENTS'
 export const GS_USERS = 'GS_USERS'
+
+
+// frontend?
+export const VIEW_ALL_GROCERY_LISTS = 'VIEW_ALL_GROCERY_LISTS'
+export const VIEW_ONE_GROCERY_LIST = 'VIEW_ONE_GROCERY_LIST'
+export const MANAGE_GROCERY = 'MANAGE_GROCERY'
+export const CLONE_GROCERY = 'CLONE_GROCERY'
+
+// change the order of elements in array
+export const SHUFFLE_GROCERY_LISTS = 'SHUFFLE_GROCERY_LISTS'
+
+export const SEARCH_BY_NAME = 'SEARCH_BY_NAME'
+export const SEARCH_BY_CONTENT = 'SEARCH_BY_CONTENT'
+export const SEARCH_BY_TYPE = 'SEARCH_BY_TYPE'
+export const SEARCH_BY_TAG = 'SEARCH_BY_TAG'
+
+// big array and pluck/chunk only that fill to our filer.
+// i.e. has peanutbutter at ingredients.
+export const SEARCH_BY_FILTER = 'SEARCH_BY_FILTER'
+
+// definately frontend, links that we passing to
+export const URL_EDIT_GROCERY = 'URL_EDIT_GROCERY'
+export const URL_VIEW_GROCERY = 'URL_VIEW_GROCERY'
+export const URL_VIEW_DEPARTMENT = 'URL_VIEW_DEPARTMENT'
+export const URL_CLONE_GROCERY = 'URL_CLONE_GROCERY'
+export const URL_DELETE_GROCERY = 'URL_DELETE_GROCERY'
+export const URL_HIDE_DEPARTMENT = 'URL_HIDE_DEPARTMENT'
+export const URL_PURCHASED_INGREDIENTS = 'URL_PURCHASED_INGREDIENTS'
+export const URL_FAVORITE_INGREDIENTS = 'URL_FAVORITE_INGREDIENTS'
+export const URL_USER_PROFILE = 'URL_USER_PROFILE'
+
+
+// all of URLS should have a method where we pass URL + id usually this is how it works.
