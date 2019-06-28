94379a7ea8e2962fdd56d37f2f088c5f7ca0ffcb
Prashant Uniyal <uniyalprashant9@gmail.com>
2019-06-28 06:52:16 +0530

    Update writeFile.js

--- generator/writeFile.js
+++ generator/writeFile.js
@@ -1,49 +1,47 @@
 // const filePath = require('../files')
 const fs = require('fs')
 const PATH = require('path')
-const srcUtils = require('./../src/utils')
+const src_utils = require('./../src/utils')
 const { promisify } = require('util') // ?? it's utils of not *** Answer : NO. It's using for writing data in json
-// const { promisify } = require('util')
-// const _ = require('lodash')
-
-const writeFilePromisify = promisify(fs.writeFile)
-
-async function writing (path, data) {
-  await writeFilePromisify(
-    path, data
-  )
-  console.info(path + ' file generated successfully!')
-}
+    // const { promisify } = require('util')
+    // const _ = require('lodash')
 
 /**
  * Write in file
  * @param {String} path
  * @param {Object} data
  */
-function writeFile (path, data) {
-  // console.log(typeof users);
-  // console.log(typeof usersStr);
-  if (typeof data === 'undefined') {
-    console.error('Error variable is undefined')
-    return
-  }
+function writeFile(path, data) {
+    // console.log(typeof users);
+    // console.log(typeof usersStr);
+    if (typeof data === 'undefined') {
+        console.error('Error variable is undefined')
+        return
+    }
 
-  if (typeof data === 'object') {
     var dataStr = JSON.stringify(data)
-    writing(path, dataStr).catch(
-      error => console.error(error)
-    )
-  }
+    dataStr = dataStr.replace(/{\"/g, '{ \"')
+    dataStr = dataStr.replace(/{\"/g, '{ \" ')
+    dataStr = dataStr.replace(/}/g, ' }')
+    dataStr = dataStr.replace(/\":/g, '\": ')
+    dataStr = dataStr.replace(/,\"/g, ',\n \"')
+    dataStr = '[' + dataStr + ']'
+    console.log(dataStr)
+    fs.writeFile(path, dataStr, function(err) {
+        if (err)
+            return console.log(err)
+        console.info(path + ' file generated successfully!')
+    })
 }
 
-function test () {
-  // console.log(filePath["groceryFilePath"]);
-  // console.log(JSON.parse("src/data/Grocery/grocery.json"));
-  // console.log(grocery);
-  // writeFiles()
-  // console.log(typeof require(filePath.groceryFilePath));
+function test() {
+    // console.log(filePath["groceryFilePath"]);
+    // console.log(JSON.parse("src/data/Grocery/grocery.json"));
+    // console.log(grocery);
+    // writeFiles()
+    // console.log(typeof require(filePath.groceryFilePath));
 
-  console.log('ok')
+    console.log('ok')
 }
 
 // execute function
@@ -53,10 +51,10 @@ function test () {
  * For fixPath()
  * @param {String} path
  */
-function fixPath (path) {
-  path = PATH.resolve(__dirname, path) // absolute path
-  if (path[-1] !== '/') { path = path + '/' } // path correction
-  return path
+function fixPath(path) {
+    path = PATH.resolve(__dirname, path) // absolute path
+    if (path[-1] !== '/') { path = path + '/' } // path correction
+    return path
 }
 
 /**
@@ -64,10 +62,10 @@ function fixPath (path) {
  * @param {string} path
  * @param {string} file
  * */
-function readData (path, file) {
-  let data = fs.readFileSync(path + file)
-  let fileData = JSON.parse(data)
-  return fileData
+function readData(path, file) {
+    let data = fs.readFileSync(path + file)
+    let fileData = JSON.parse(data)
+    return fileData
 }
 
 /**
@@ -76,46 +74,51 @@ function readData (path, file) {
  * @param {Object} fileData
  * @param {var} flag
  * */
-function saveFile (folderNamePath, file, fileData, flag) {
-  var fileDataLength = fileData.length
-  for (var i = 0; i < fileDataLength; i++) {
-    var fileName = getFileName(file, fileData[i], flag, i)
-    var elementPath = folderNamePath + '/' + fileName
-    writeFile(elementPath, fileData[i])
-  }
+function saveFile(folderNamePath, file, fileData, flag) {
+    var fileDataLength = fileData.length
+    for (var i = 0; i < fileDataLength; i++) {
+        var fileName = getFileName(file, fileData[i], flag, i)
+        var elementPath = folderNamePath + '/' + fileName
+        var data = fileData[i]
+        writeFile(elementPath, fileData[i])
+    }
 }
 
 /**
  * @param {String} path
  * @param {String} file
  */
-function makeFolder (path, file) {
-  var folderName = file.slice(0, -5) + '_elements'
-  var folderNamePath = path + folderName
-  if (isDirectory(folderNamePath)) {
-    fs.mkdirSync(folderNamePath)
-  }
-  return folderNamePath
+function makeFolder(path, file) {
+    var folderName = file.slice(0, -5) + '_elements'
+    var folderNamePath = path + folderName
+    if (isDirectory(folderNamePath)) {
+        fs.mkdirSync(folderNamePath)
+    }
+    return folderNamePath
 }
 /**
  * For splitObject
  * @param {String} path
  * @param {String} file
  * @param {var} flag
+ * @param {var} callback
+ * @param {var} keys
  */
-function splitObject (path, file, flag = 1) { // split large files into single elements
-  /*
-      flag=1 ==> name according to index
-      flag=0 ==> name according to "name" attribute
-    */
-  if (file.slice(-5) !== '.json') {
+function splitObject(path, file, flag = 1, callback, keys = []) { // split large files into single elements
+    /*
+        flag=1 ==> name according to index
+        flag=0 ==> name according to "name" attribute
+      */
+    if (file.slice(-5) !== '.json') {
         console.log("Require .json file.")
         return
-  }
-  path = fixPath(path)
-  let fileData = readData(path, file) // Reading data...
-  var folderNamePath = makeFolder(path, file) // new folder to save splitted files
-  saveFile(folderNamePath, file, fileData, flag) // saving files
+    }
+    path = fixPath(path)
+    let fileData = readData(path, file) // Reading data...
+    var folderNamePath = makeFolder(path, file) // new folder to save splitted files
+    saveFile(folderNamePath, file, fileData, flag) // saving files
+    if (callback instanceof Function)
+        callback(folderNamePath, keys)
 }
 // execute function
 // splitObject()
@@ -124,21 +127,21 @@ function splitObject (path, file, flag = 1) { // split large files into single e
  * fixFileName()
  * @param {string} fileName
  */
-function fixFileName (fileName) {
-  fileName = fileName.replace(/ /g, '_') // Replace space with underscore
-  fileName = fileName.toLowerCase() // Maintain Uniformity
-  return fileName
+function fixFileName(fileName) {
+    fileName = fileName.replace(/ /g, '_') // Replace space with underscore
+    fileName = fileName.toLowerCase() // Maintain Uniformity
+    return fileName
 }
 
 /**
  * isDirectory()
  * @param {string} folderNamePath
  *  */
-function isDirectory (folderNamePath) {
-  if (fs.existsSync(folderNamePath)) {
-    return false
-  }
-  return true
+function isDirectory(folderNamePath) {
+    if (fs.existsSync(folderNamePath)) {
+        return false
+    }
+    return true
 }
 
 /**
@@ -148,14 +151,13 @@ function isDirectory (folderNamePath) {
  * @param {var} flag
  * @param {var} index
  */
-function getFileName (file, fileData, flag, index) {
-  var fileName
-  if (flag === 1) fileName = index + '-' + file // for example: 23-someJsonFile.json
-  else fileName = fileData.name + '.json' // for example: someValueOfName.json
-  fileName = fixFileName(fileName)
-  return fileName
+function getFileName(file, fileData, flag, index) {
+    var fileName
+    if (flag === 1) fileName = index + '-' + file // for example: 23-someJsonFile.json
+    else fileName = fileData.name + '.json' // for example: someValueOfName.json
+    fileName = fixFileName(fileName)
+    return fileName
 }
-
 
 /**
  * For combineObjects()
@@ -164,7 +166,7 @@ function getFileName (file, fileData, flag, index) {
  */
 function combineObject(path, keys) {
     path = fixPath(path)
-    var content = srcUtils.readAllFiles(path) //read all json files
+    var content = src_utils.readAllFiles(path) //read all json files
     content = updateContent(content, keys) //modifying structure
     var fileNamePath = path + PATH.basename(path) + "_combined.json" // for example: elements_combined,json
     writeFile(fileNamePath, content) //saving
@@ -177,7 +179,6 @@ function combineObject(path, keys) {
  */
 function updateContent(content, keys) {
     var len = content.length
-    //console.log(len)
     for (var itr = 0; itr < len; itr++) {
         var elementLen = content[itr].length
         for (var i = 0; i < elementLen; i++) {
