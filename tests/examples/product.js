c92757df834b9e63ae563076099b886455cc96eb
Andrew Serra <andy@serra.us>
2019-06-28 15:42:53 +0300

    added tests for serving size and product

---
+++ tests/examples/product.js
@@ -0,0 +1,60 @@
+const schema = {
+  properties: {
+    NDB_Number: {
+      type: 'string'
+    },
+    long_name: {
+      type: 'string'
+    },
+    data_source: {
+      type: 'string'
+    },
+    gtin_upc: {
+      type: 'string'
+    },
+    manufacturer: {
+      type: 'string'
+    },
+    date_modified: {
+      type: 'string'
+    },
+    date_available: {
+      type: 'string'
+    },
+    ingredients_english: {
+      type: 'string'
+    }
+  },
+  required: [
+    "NDB_Number",
+    "long_name",
+    "data_source",
+    "gtin_upc",
+    "manufacturer",
+    "date_modified",
+    "date_available",
+    "ingredients_english"
+  ]
+}
+
+const example = {
+  "NDB_Number":"45001524",
+  "long_name":"MOCHI ICE CREAM BONBONS",
+  "data_source":"LI",
+  "gtin_upc":"019022128593",
+  "manufacturer":"G. T. Japan, Inc.",
+  "date_modified":"11/15/2017",
+  "date_available":"11/15/2017",
+  "ingredients_english":"ICE CREAM INGREDIENTS: MILK, CREAM, SUGAR, STRAWBERRIES \
+                        (STRAWBERRIES, SUGAR), CORN SYRUP SOLIDS, SKIM MILK, WHEY, \
+                        NATURAL FLAVOR, GUAR GUM, MONO & DIGLYCERIDES, BEET JUICE \
+                        AND BEET POWDER (FOR COLOR), CELLULOSE GUM, LOCUST BEAN GUM, \
+                        CARRAGEENAN. COATING INGREDIENTS: SUGAR, WATER, RICE FLOUR, \
+                        TREHALOSE, EGG WHITES, BEET JUICE AND BEET POWDER (FOR COLOR), \
+                        DUSTED WITH CORN & POTATO STARCH"
+}
+
+module.exports = {
+  schema,
+  example
+}
---
+++ tests/product.test.js
@@ -0,0 +1,33 @@
+/* global describe, it, expect */
+'use strict'
+const path = require('path')
+const { getFileInfo } = require('../src/utils')
+const { schema, example } = require('./examples/product')
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
+const pathJson = '../generator/projects/USFA/Product'
+let files = getFileInfo(pathJson)
+
+console.log(files.length)
+
+describe('product data files returns a path', () => {
+  files.forEach((file) => {
+    if (path.extname(file) !== '.js') {
+      let filePath = path.resolve(__dirname, pathJson, file)
+      it(`prevents any issues and problems product file: ${file}`, () => {
+        expect(filePath).not.toBe('')
+      })
+    }
+  })
+})
+
+try {
+  describe('test product json', () => {
+    it('validates my json', () => {
+      expect(example).toMatchSchema(schema)
+    })
+  })
+} catch (e) {
+  console.log(`${e.name}: ${e.message}`)
+}
