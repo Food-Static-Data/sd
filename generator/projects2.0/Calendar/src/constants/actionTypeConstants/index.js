5e61b64c22372e72bc0a3409468c762906eec07c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-29 21:18:46 +0300

    constants at sd

---
+++ generator/projects2.0/Calendar/src/constants/actionTypeConstants/index.js
@@ -0,0 +1,17 @@
+export const VIEW_CALENDAR = 'VIEW_CALENDAR'
+export const VIEW_DAY = 'VIEW_DAY'
+export const VIEW_WEEK = 'VIEW_WEEK'
+export const VIEW_DETAILS = 'VIEW_DETAILS'
+export const CHANGE_DATE = 'CHANGE_DATE'
+export const CHANGE_TIME = 'CHANGE_TIME'
+
+export const URL_FULL_CALENDAR = 'URL_FULL_CALENDAR'
+export const URL_ONE_DAY = 'URL_ONE_DAY'
+export const URL_DETAILS = 'URL_DETAILS'
+export const URL_ADD_EVENT = 'URL_ADD_EVENT'
+export const URL_DELETE_EVENT = 'URL_DELETE_EVENT'
+export const URL_EDIT_EVENT  = ''
+
+export const FILES_RECIPE = 'URL_EDIT_EVENT'
+export const FILES_CALENDAR = 'FILES_CALENDAR'
+export const FILES_WEEKLY_MENU = 'FILES_WEEKLY_MENU'
