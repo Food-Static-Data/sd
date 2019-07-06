37b65e1ca3c5b4d311f39e80e8aab8307d1a5678
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-06 16:32:10 +0300

    changes

--- generator/utils.js
+++
@@ -1,6 +0,0 @@
-// @TODO here we'll store all necessary methods that we can use at different places.
-
-// i think we should have here methods like
-// __generateId
-// __generateId
-// getFileKey - i don't like the name
