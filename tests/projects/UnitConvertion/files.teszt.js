464647532ff8949a89f82bdaeb35d2ad9ee7f5b1
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-07-11 05:47:22 +0300

    removing part of tests, because they should be seprated. something may not work well

--- tests/projects/UnitConvertion/files.teszt.js
+++
@@ -1,8 +0,0 @@
-// /* global test, describe, it, expect, jest */
-// const { __generateId } = require('@utils')
-
-// describe('empty test', () => {
-//   test.skip('it is not snowing', () => {
-//     expect(false).toBe(0)
-//   })
-// })
