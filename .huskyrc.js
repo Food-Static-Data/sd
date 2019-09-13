4e144efbced578d41f61b3fa08cf30454559cd51
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-09-13 22:06:26 +0000

    pkg install

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,11 @@
-//   "husky": {
 //     "hooks": {
 //       "pre-commit": "npm run clean && npm run formato",
 //       "after": "lint-staged",
 //       "then": "yarn precommit"
 //     }
-//   },
+module.exports = {
+  hooks: {
+    "pre-commit": "npm run clean && npm run formato",
+    after: "lint-staged -c lint-staged.config.js"
+  }
+};
