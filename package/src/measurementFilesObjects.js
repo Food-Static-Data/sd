ef3b7308e473204d925b8bf9247bde807522daaf
Edebo <akandeadebowale0@gmail.com>
2019-11-11 11:49:23 +0100

    adjusting eslint warming sign in src/measurementFilesObjects.js

--- package/src/measurementFilesObjects.js
+++ package/src/measurementFilesObjects.js
@@ -1,4 +1,4 @@
-const jsonFiles = require('./measurementFiles');
+const jsonFiles = require("./measurementFiles");
 // const {
 //   measurementSystem,
 //   measurementUnits,
@@ -6,23 +6,23 @@ const jsonFiles = require('./measurementFiles');
 
 let arr;
 
-Object.keys(jsonFiles).forEach((key) => {
+Object.keys(jsonFiles).forEach(key => {
   arr = {
     ...arr,
-    [key]: require(jsonFiles[key]),
+    [key]: require(jsonFiles[key])
   };
 });
 
-const getMeasurementSystem = function () {
-  return (arr.measurementSystem);
+const getMeasurementSystem = () => {
+  return arr.measurementSystem;
 };
 
-const getMeasurementUnits = function () {
-  return (arr.measurementUnits);
+const getMeasurementUnits = () => {
+  return arr.measurementUnits;
 };
 
 module.exports = {
   arr,
   getMeasurementSystem,
-  getMeasurementUnits,
+  getMeasurementUnits
 };
