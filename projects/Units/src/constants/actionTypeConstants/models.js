488adc7d57d5cd426dc33ae68c32ff7d97a95e41
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-11-17 03:31:11 +0000

    1

--- projects/Units/src/constants/actionTypeConstants/models.js
+++
@@ -1,4 +0,0 @@
-// export const UNITS_DEPARTMENTS = 'GS_DEPARTMENTS'
-// export const UNITS_GROCERY = 'GS_GROCERY'
-// export const UNITS_INGREDIENTS = 'GS_INGREDIENTS'
-// export const UNITS_USERS = 'GS_USERS'
