7c76c6447087b61cc700c8f42cb9d42af46dc2c3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-11-17 03:29:22 +0000

    1

--- projects/FoodAttributes/src/constants/actionTypeConstants/models.js
+++
@@ -1,4 +0,0 @@
-// export const FA_DEPARTMENTS = 'GS_DEPARTMENTS'
-// export const FA_GROCERY = 'GS_GROCERY'
-// export const FA_INGREDIENTS = 'GS_INGREDIENTS'
-// export const FA_USERS = 'GS_USERS'
