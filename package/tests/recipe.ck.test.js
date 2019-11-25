d16116a286d67261ab2bd452460203d82237fd28
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-11-25 07:11:37 +0200

    clean up

--- package/tests/recipe.ck.test.js
+++
@@ -1,9 +0,0 @@
-/* global describe, it, expect */
-'use strict'
-const { recipeCk } = require('@files')
-
-describe('recipeCk data files returns array', () => {
-  it('these tests prevent any issues and problems, also to break the structure of recipeCk', () => {
-    expect(recipeCk).not.toBe('')
-  })
-})
